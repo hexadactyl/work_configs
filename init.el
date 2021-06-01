41515383efca20d62c1940c6ce65f371465aef43
hexadactyl <mattspromotions@protonmail.com>
2021-06-01 13:36:06 -0400

    updated emacs config

--- init.el
+++ init.el
@@ -6,10 +6,18 @@
 (menu-bar-mode -1)
 (setq visible-bell t)
 
-(setq x-select-enable-clipboard t)
+;; HOW THE FUCK DOES THE GOD DAMN CLIPBOARD WORK
+;(setq x-select-enable-clipboard t)
 
 ;; make escape work like it should
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; set tab width
+(setq-default tab-width 2)
+(setq-default evil-shift-width tab-width)
+
+;; use spaces for indentation
+(setq-default indent-tabs-mode nil)
 
 ;; initialize package sources
 (require 'package)
@@ -37,6 +45,19 @@
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
+;; diminish allows hiding modes in the modeline
+(use-package diminish)
+
+(use-package org)
+
+(use-package telephone-line
+  :init (telephone-line-mode)
+  :custom
+  (telephone-line-primary-left-separator 'telephone-line-identity-left)
+  (telephone-line-secondary-left-separator 'telephone-line-identity-hollow-left)
+  (telephone-line-primary-right-separator 'telephone-line-identity-right)
+  (telephone-line-secondary-right-separator 'telephone-line-identity-hollow-right))
+
 (use-package ivy
   :init (ivy-mode)
   :diminish ivy-mode
@@ -52,9 +73,44 @@
          ("C-d" . ivy-switch-buffer-kill)
          :map ivy-reverse-i-search-map
          ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
+         ("C-d" . ivy-reverse-i-search-kill)))
+
+; enable fuzzy finding EVERYWHERE
+(setq ivy-re-builders-alist
+      '((t . ivy--regex-fuzzy)))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package ivy-hydra
+  :defer t
+  :after hydra)
+
+(use-package hydra
+  :defer 1)
+
+(defhydra hydra/cycle-buffers (:timeout 15)
+  "cycle buffers"
+  ("j" next-buffer)
+  ("k" previous-buffer)
+  ("x" kill-this-buffer)
+  ("c" kill-buffer-and-window)
+  ("SPC" nil "quit" :exit t))
+
+(defhydra hydra/resize-window (:timeout 15)
+  "resize window"
+  ("=" evil-window-increase-height)
+  ("-" evil-window-decrease-height)
+  ("." evil-window-increase-width)
+  ("," evil-window-decrease-width)
+  ("SPC" nil "quit" :exit t))
+
+(defhydra hydra/text-zoom (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("SPC" nil "quit" :exit t))
 
 (use-package doom-themes
   :init (load-theme 'doom-gruvbox t))
@@ -65,17 +121,45 @@
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
+  ;:config
+  ;(setq which-key-idle-delay 0.3))
+  )
 
-(use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
+(use-package general
+  :config
+  (general-create-definer matt/leader-keys
+			  :keymaps '(normal insert visual emacs)
+			  :prefix "SPC"
+			  :global-prefix "C-SPC")
+  (matt/leader-keys
+    "b"   '(:ignore t :which-key "buffers")
+    "bb"  '(hydra/cycle-buffers/body :which-key "cycle buffers")
+    "bk"  'kill-buffer-and-window
+
+    "w"   '(:ignore t :which-key "windows")
+    "wr"  '(hydra/resize-window/body :which-key "resize window")
+
+    "z"   '(hydra/text-zoom/body :which-key "zoom text")
+
+    "g"   '(:ignore t :which-key "git")
+    "gs"  'magit-status
+    "gd"  'magit-diff-unstaged
+    "gc"  'magit-branch-or-checkout
+    "gl"  '(:ignore t :which-key "log")
+    "glc" 'magit-log-current
+    "glf" 'magit-log-buffer-file
+    "gb"  'magit-branch
+    "gP"  'magit-push-current
+    "gp"  'magit-pull-branch
+    "gf"  'magit-fetch
+    "gF"  'magit-fetch-all
+    "gr"  'magit-rebase))
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
          ("C-x b" . counsel-ibuffer)
          ("C-x C-f" . counsel-find-file)
+         ("C-x z" . counsel-fzf)
          :map minibuffer-local-map
          ("C-r" . 'counsel-minibuffer-history)))
 
@@ -112,8 +196,23 @@
   :config
   (evil-collection-init))
 
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :bind-keymap ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/projects")
+    (setq projectile-project-search-path '("~/projects")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;(use-package evil-magit
+;  :after magit)
+
 (with-eval-after-load 'dired
-  ;;(define-key dired-mode-map (kbd "M-p") 'peep-dired)
   (evil-define-key 'normal dired-mode-map (kbd "h") 'dired-up-directory)
   (evil-define-key 'normal dired-mode-map (kbd "l") 'dired-find-file) ; use dired-find-file instead if not using dired-open package
   (evil-define-key 'normal peep-dired-mode-map (kbd "j") 'peep-dired-next-file)
@@ -122,26 +221,133 @@
 (use-package slime)
 (setq inferior-lisp-program "clisp")
 
-(use-package clojure-mode)
+(use-package racket-mode)
+
+(use-package clojure-mode
+  :ensure t
+  :hook (clojure-mode . lsp-deferred))
 (use-package queue)
 (use-package cider)
 
+(use-package python-mode
+  :ensure t
+  :hook (python-mode . lsp-deferred) 
+  :custom (python-shell-interpreter "python3"))
 
-;(use-package lispy
-  ;:hook ((emacs-lisp-mode
-	 ;lisp-mode
-	 ;lisp-interaction-mode
-	 ;slime-repl-mode) . lispy-mode))
-         ;clojure-mode
+(require 'ob-js)
 
-;(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+(defun matt/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-ssegments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook (lsp-mode . matt/lsp-mode-setup)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration t))
+
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+          ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+          ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode))
+
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+;; Org Mode Configuration ------------------------------------------------------
+
+(defun efs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+  (efs/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun efs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . efs/org-mode-visual-fill))
+
+(org-babel-do-load-languages
+  'org-babel-load-languages
+  '((emacs-lisp . t)
+    (lisp . t)
+    ;(scheme . t)
+    (python . t)
+    (js . t)
+    ))
+
+(with-eval-after-load 'org
+  ;; This is needed as of Org 9.2
+  (require 'org-tempo)
+
+  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist '("cl" . "src lisp"))
+  (add-to-list 'org-structure-template-alist '("js" . "src js"))
+  (add-to-list 'org-structure-template-alist '("py" . "src python")))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ivy-rich which-key use-package rainbow-delimiters ivy)))
+ '(custom-safe-themes
+   '("a41d7d4c20bfa90be5450905a69f65a8ae35d3bcb97f11dfaef47036cf72a372" default))
+ '(helm-minibuffer-history-key "M-p")
+ '(package-selected-packages
+   '(lsp-ui company-box company python-mode lsp-mode telephone-line ivy-rich which-key use-package rainbow-delimiters ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
